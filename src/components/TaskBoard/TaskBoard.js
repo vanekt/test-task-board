7a5317ceed3a3ba045f1849f0f53da19ab3442b3
Ivan Tkachenko <vanekt@yandex.ru>
2018-08-11 00:09:35 +0300

    use react-dnd

--- src/components/TaskBoard/TaskBoard.js
+++ src/components/TaskBoard/TaskBoard.js
@@ -1,16 +1,72 @@
 import React from 'react';
-import Task from '../Task'
+import { DragDropContext } from 'react-dnd'
+import HTML5Backend from 'react-dnd-html5-backend'
+import Task from '../Task';
 import './TaskBoard.css';
+import update from 'immutability-helper'
 
-const arr = [];
-for (let i = 0; i < 12; i++) {
-    arr.push(i)
+class TaskBoard extends React.Component {
+    state = {
+        cards: [
+            {
+                id: 1,
+                text: 'Write a cool JS library',
+            },
+            {
+                id: 2,
+                text: 'Make it generic enough',
+            },
+            {
+                id: 3,
+                text: 'Write README',
+            },
+            {
+                id: 4,
+                text: 'Create some examples',
+            },
+            {
+                id: 5,
+                text: 'Spam in Twitter and IRC to promote it (note that this element is taller than the others)',
+            },
+            {
+                id: 6,
+                text: '???',
+            },
+            {
+                id: 7,
+                text: 'PROFIT',
+            },
+        ]
+    };
+
+    render() {
+        return (
+            <div className="TaskBoard">
+                {this.state.cards.map((task, i) => (
+                    <Task
+                        key={task.id}
+                        index={i}
+                        id={task.id}
+                        data={task}
+                        moveCard={this.moveCard}
+                    />
+                ))}
+            </div>
+        )
+    }
+
+    moveCard = (dragIndex, hoverIndex) => {
+        const { cards } = this.state;
+        const dragCard = cards[dragIndex];
+
+        this.setState(
+            update(this.state, {
+                cards: {
+                    $splice: [[dragIndex, 1], [hoverIndex, 0, dragCard]],
+                },
+            }),
+        )
+    }
 }
 
-export default () => (
-    <div className="TaskBoard">
-        {arr.map((i) => {
-          return  <Task key={i} data={i} />
-        })}
-    </div>
-)
+export default DragDropContext(HTML5Backend)(TaskBoard)
